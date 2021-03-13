2a658ad5c5ae13b465999e64316af3978771e065
Bashar Khouri <Bashar.19002@hotmail.com>
2021-03-13 14:23:23 +0200

    added some Models

---
+++ Source Code/LSS/LSS/Models/Faculty.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LSS.Models
+{
+    public class Faculty
+    {
+        public String Name { get; set; }
+        public Dean Dean { get; set; }
+        
+
+    }
+}
---
+++ Source Code/LSS/LSS/Models/Lecturer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LSS.Models
+{
+    public class Lecturer
+    {
+        public String Name { get; set; }
+        public String Password { get; set; }
+    }
+}
