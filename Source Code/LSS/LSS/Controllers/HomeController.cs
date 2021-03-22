88b3f140957f6489f6998ff3895d56d4786bf158
Bashar Khouri <Bashar.19002@hotmail.com>
2021-03-22 18:41:46 +0200

    .

--- Source Code/LSS/LSS/Controllers/HomeController.cs
+++ Source Code/LSS/LSS/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace LSS.Controllers
             return View();
         }
 
+        public ActionResult Login()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
