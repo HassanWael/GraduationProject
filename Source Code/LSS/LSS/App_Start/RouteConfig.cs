16382167ce8a78b330e943d2db451df7cd572b1e
Bashar Khouri <Bashar.19002@hotmail.com>
2021-04-09 18:27:23 +0300

    Created DB models in C#
    #ToDo deleted dub models

--- Source Code/LSS/LSS/App_Start/RouteConfig.cs
+++ Source Code/LSS/LSS/App_Start/RouteConfig.cs
@@ -15,7 +15,7 @@ namespace LSS
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
