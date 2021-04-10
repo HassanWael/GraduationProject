5b6d1c97766a2262b3864135a131e7225648e1db
Bashar Khouri <Bashar.19002@hotmail.com>
2021-04-11 00:35:25 +0300

    Updated NuGet packages

--- Source Code/LSS/LSS/Controllers/HomeController.cs
+++ Source Code/LSS/LSS/Controllers/HomeController.cs
@@ -1,23 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using LSS.Models.LSS_DB_Model;
 
 namespace LSS.Controllers
 {
     public class HomeController : Controller
     {
-    
+        public LSS_databaseEntities _dbEntities = new LSS_databaseEntities(); 
+
         public ActionResult Login()
         {
             return View();
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(String ID , String password)
+        {
+            if (ModelState.IsValid)
+            {
+                var data = _dbEntities.Lecturers.Where(s => s.ID.Equals(ID) && s.Password.Equals(password)).ToList();
+                if (data.Count > 0)
+                {
+                    Session["Name"] = data.FirstOrDefault().Name;
+                    Session["ID"] = data.FirstOrDefault().ID;
+                    return RedirectToAction("CoursePagee");
+                }
+                else
+                {
+                    ViewBag.error = "Login Failed";
+                    return RedirectToAction("Login");
+                }
+            }
+
+
+            return View();
+        }
+  
+
+
         public ActionResult CouresPage() {
 
-            ViewBag.Message = " Coures view Page";
-            return View();
+            if (Session["ID"] == null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }        
         }
     }
 }
