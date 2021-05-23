f4e6621ce2dd1d8f6a5621edbbfd4b1eb123db8f
Bashar Khouri <Bashar.19002@hotmail.com>
2021-05-23 23:07:34 +0300

    reuploaded ActionsForImroving

--- Source Code/LSS/LSS/Controllers/CourseCoordinatorController.cs
+++ Source Code/LSS/LSS/Controllers/CourseCoordinatorController.cs
@@ -100,7 +100,7 @@ namespace LSS.Controllers
         }
 
 
-        public ActionResult ActionsForImproving(string courseId)
+        public ActionResult ActionsForImproving(string? courseId)
         {
             List<ActionsForImprovingTheCourse> AFITC = _DatabaseEntities.ActionsForImprovingTheCourses.Where(x => x.CourseID.Equals(courseId)).ToList();
             ViewBag.courseId = courseId;
